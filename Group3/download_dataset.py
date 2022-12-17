2a19b9b8f1f65d0a4afb9954a04348b6d26d7ba8
Dhrushit Raval <dr9703@rit.edu>
2022-12-17 00:44:48 -0500

    get one value

--- Group3/download_dataset.py
+++ Group3/download_dataset.py
@@ -1,9 +1,12 @@
 from __future__ import print_function
 import os
+import json
+import pretty_json
 
 PYOACORE_KEY = "RDGVzeO2d6jgNHFyaoXfupK9WSI5Bh4k"
 os.environ["CORE_API_KEY"] = PYOACORE_KEY
 import time
+import requests
 import swagger_client
 import pyoacore
 from pyoacore.rest import ApiException
@@ -12,8 +15,29 @@ from pprint import pprint
 api_instance = pyoacore.ArticlesApi()
 core_id = '42' # str | The id of the article
 
-try:
-    # Get articles by ID
-    api_instance.articles_get_core_id_get(core_id)
-except ApiException as e:
-    print("Exception when calling ArticlesApi->articles_get_core_id_get: %s\n" % e)
+# try:
+#     # Get articles by ID
+#     api_instance.articles_get_core_id_get(core_id)
+# except ApiException as e:
+#     print("Exception when calling ArticlesApi->articles_get_core_id_get: %s\n" % e)
+
+
+api_key = "RDGVzeO2d6jgNHFyaoXfupK9WSI5Bh4k"
+# with open ("apikey.txt", "r") as apikey_file:
+#     api_key=apikey_file.readlines()[0].strip()
+api_endpoint = "https://api.core.ac.uk/v3/"
+
+
+def get_entity(url_fragment):
+    headers={"Authorization":"Bearer "+api_key}
+    response = requests.get(api_endpoint + url_fragment, headers=headers)
+    if response.status_code == 200:
+        return response.json(), response.elapsed.total_seconds()
+    else:
+        print(f"Error code {response.status_code}, {response.content}")
+
+data_provider, elapsed = get_entity("data-providers/1")
+# pretty_json(data_provider)
+json_formatted_str = json.dumps(data_provider, indent=2)
+
+print(json_formatted_str)
