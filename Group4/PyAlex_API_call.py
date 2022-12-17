01a97afe2ebe542cbdcd32706da0bd2928ff620b
xavier-tarr <63113329+xavier-tarr@users.noreply.github.com>
2022-12-17 12:21:31 -0500

    formatting paper based on requirements discussed
    Still need:
    #Need to pull H_index from authorships
    #Need the count from referenced_works
    #Need counts by year cumulative for five years after publication

--- Group4/PyAlex_API_call.py
+++ Group4/PyAlex_API_call.py
@@ -11,16 +11,31 @@ import json
 
 pyalex.config.email = "xavier.tarr@gmail.com"
 
-pager = Works().search_filter(display_name="Boiling").paginate(per_page=26)
-papers = []
+#pager = Works().search_filter(publication_year=2017,is_retracted=0 ).paginate(per_page=26)
+pager_2017 = Works().filter(publication_year=2017, is_retracted=False, is_paratext=False).paginate(per_page=200)
+pager_2016 = Works().filter(publication_year=2016, is_retracted=False, is_paratext=False).paginate(per_page=200)
+pager_2015 = Works().filter(publication_year=2015, is_retracted=False, is_paratext=False).paginate(per_page=200)
+pager_2014 = Works().filter(publication_year=2014, is_retracted=False, is_paratext=False).paginate(per_page=200)
+pager_2013 = Works().filter(publication_year=2013, is_retracted=False, is_paratext=False).paginate(per_page=200)
 
-for page in pager:
+papers = []
+paper_formatted = []
+
+#Need to pull H_index from authorships
+#Need the count from referenced_works
+#Need counts by year cumulative for five years after publication
+
+
+for page in pager_2017:
     for paper in page:
-        papers.append(paper)
+        paper_formatted = [paper['id'], paper['doi'],paper['title'],paper['publication_year'],paper['host_venue'],
+                      paper['type'],paper['open_access'],paper['authorships'],paper['concepts'],paper['referenced_works'],
+                      paper['counts_by_year'],paper['abstract']]
+        papers.append(paper_formatted)
         if len(papers) > 25:
             break
     if len(papers) > 25:
         break
 
-with open('data2.json', 'w') as f:
-    json.dump(papers, f)
+# with open('data3.json', 'w') as f:
+#     json.dump(papers, f)
